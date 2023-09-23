b631eed822f8d24c5220cfdc1b99f80e7acef55c
muzaim <muzaim.surya@yahoo.co.id>
2023-09-23 12:20:25 +0700

    dasdas

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://ngendika-fe.vercel.app/"
+    origins "*"
 
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head]
-      # methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
